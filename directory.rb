1d62c87c2084e82b724398bb1bfd30432d6d929d
Chuan Cheng <c_chuan23@hotmail.com>
2019-07-19 18:24:30 +0100

    Add student input method

--- directory.rb
+++ directory.rb
@@ -1,17 +1,15 @@
-# Put students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Enter the name of the students"
+  puts "To finish, hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 def print_header
   puts "The students of Villains Academy"
   puts "--------------"
@@ -25,6 +23,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 # Nothing happens until we call the methods
+students = input_students()
 print_header()
 print(students)
 print_footer(students)
