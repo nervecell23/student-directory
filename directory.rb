8b03df6a47408d2d1dcdf7c51ea273fdfb94867c
Chuan Cheng <c_chuan23@hotmail.com>
2019-07-19 22:35:49 +0100

    Modified to print names with specified letter

--- directory.rb
+++ directory.rb
@@ -10,20 +10,31 @@ def input_students
   end
   students
 end
+
 def print_header
   puts "The students of Villains Academy"
   puts "--------------"
 end
+
 def print(names)
   names.each_with_index do |name, index|
     puts "#{index+1}.#{name[:name]} (#{name[:cohort]} cohort)"
   end
 end
+
+def print_wrt_firstletter(students)
+  puts "Choose a letter"
+  letter = gets.chomp
+  print_header()
+  students.select{ |student| student[:name][0] == letter}.each do |selected_student|
+    puts "#{selected_student[:name]} (#{selected_student[:cohort]} cohort)"
+  end
+end
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 # Nothing happens until we call the methods
 students = input_students()
-print_header()
-print(students)
+print_wrt_firstletter(students)
 print_footer(students)
