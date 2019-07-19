4c88e08338e1ea3d992bcb798e32e64adf8e1062
Chuan Cheng <c_chuan23@hotmail.com>
2019-07-19 23:14:37 +0100

    Add extra info of students

--- directory.rb
+++ directory.rb
@@ -1,12 +1,16 @@
 def input_students
-  puts "Enter the name of the students"
+  puts "Enter the name of the students, their hobbies, country of birth with space"
   puts "To finish, hit return twice"
   students = []
-  name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
+  info = gets.chomp
+  while !info.empty? do
+    info_split = info.split(" ")
+    name = info_split[0]
+    hobby = info_split[1]
+    country = info_split[2]
+    students << {name: name, hobby: hobby, country: country, cohort: :november}
     puts "Now we have #{students.count} students"
-    name = gets.chomp
+    info = gets.chomp
   end
   students
 end
@@ -20,7 +24,7 @@ def print(names)
   print_header()
   idx = 0
   while idx < names.count do
-    puts "#{names[idx][:name]} (#{names[idx][:cohort]} cohort)"
+    puts "#{names[idx][:name]} (#{names[idx][:cohort]} cohort) from #{names[idx][:country]} loves #{names[idx][:hobby]}"
     idx += 1
   end
 end
